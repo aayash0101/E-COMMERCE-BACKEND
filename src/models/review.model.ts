7f44dc545d2eaf3f70b7ec1436766461925cf2b0
Aayash <dangaayash@gmail.com>
2026-06-30 13:35:08 +0545

    review model

--- src/models/review.model.ts
+++ src/models/review.model.ts
@@ -1,14 +1,50 @@
-import { SchemaOptions } from 'mongoose';
+import { Schema, model, Document, Types } from 'mongoose';
+import { baseSchemaOptions } from './base.schema';
 
-export const baseSchemaOptions: SchemaOptions = {
-    timestamps: true,
-    toJSON: {
-        virtuals: true,
-        transform: (_doc, ret: Record<string, unknown>) => {
-            ret.id = ret._id;
-            delete ret._id;
-            delete ret.__v;
-            return ret;
-        },
+export interface IReview extends Document {
+  _id: Types.ObjectId;
+  productId: Types.ObjectId;
+  customerId: Types.ObjectId;
+  orderId: Types.ObjectId; 
+  rating: number;
+  comment: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const reviewSchema = new Schema<IReview>(
+  {
+    productId: {
+      type: Schema.Types.ObjectId,
+      ref: 'Product',
+      required: true,
+      index: true,
     },
-};
+    customerId: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    orderId: {
+      type: Schema.Types.ObjectId,
+      ref: 'Order',
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 5,
+    },
+    comment: {
+      type: String,
+      required: true,
+      maxlength: 1000,
+    },
+  },
+  baseSchemaOptions
+);
+
+reviewSchema.index({ productId: 1, customerId: 1 }, { unique: true });
+
+export const Review = model<IReview>('Review', reviewSchema);
