1c2636a17bf48a7a112b943f4063b81a867c5956
Aayash <dangaayash@gmail.com>
2026-06-30 13:36:55 +0545

    small bug fix

--- src/models/base.schema.ts
+++ src/models/base.schema.ts
@@ -1,14 +1,12 @@
-import { SchemaOptions } from 'mongoose';
-
-export const baseSchemaOptions: SchemaOptions = {
-    timestamps: true,
-    toJSON: {
-        virtuals: true,
-        transform: (_doc, ret: Record<string, unknown>) => {
-            ret.id = ret._id;
-            delete ret._id;
-            delete ret.__v;
-            return ret;
-        },
+export const baseSchemaOptions = {
+  timestamps: true,
+  toJSON: {
+    virtuals: true,
+    transform: (_doc: unknown, ret: Record<string, unknown>) => {
+      ret.id = ret._id;
+      delete ret._id;
+      delete ret.__v;
+      return ret;
     },
+  },
 };
