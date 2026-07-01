9c084f136eed28585c4578240a1dfff691de7e87
Aayash <dangaayash@gmail.com>
2026-07-01 14:28:56 +0545

    order routes

---
+++ src/modules/orders/order.routes.ts
@@ -0,0 +1,50 @@
+import { Router } from 'express';
+import { orderController } from './order.controller';
+import { protect } from '@middlewares/auth.middleware';
+import { restrictTo } from '@middlewares/restrictTo.middleware';
+import { requireApprovedVendor } from '@middlewares/requireApprovedVendor.middleware';
+import { validate } from '@middlewares/validate.middleware';
+import { placeOrderSchema, updateItemStatusSchema } from './order.validation';
+
+const router = Router();
+
+router.post(
+    '/',
+    protect,
+    restrictTo('customer'),
+    validate(placeOrderSchema),
+    orderController.placeOrder
+);
+
+
+router.get(
+    '/',
+    protect,
+    restrictTo('customer'),
+    orderController.getMyOrders
+);
+
+router.get(
+    '/vendor/mine',
+    protect,
+    restrictTo('vendor'),
+    requireApprovedVendor,
+    orderController.getVendorOrders
+);
+
+router.put(
+    '/:id/items/:itemId',
+    protect,
+    restrictTo('vendor'),
+    requireApprovedVendor,
+    validate(updateItemStatusSchema),
+    orderController.updateItemStatus
+);
+
+router.get(
+    '/:id',
+    protect,
+    orderController.getOrderById
+);
+
+export default router;
